4957b67b498d28272ef63ebb237130dab17684ca
Mike Bierlee <m.bierlee@lostmoment.com>
2014-05-27 02:02:07 +0200

    Test that you can autowire members declared as an interface type

--- test/poodinis/autowiretest.d
+++ test/poodinis/autowiretest.d
@@ -12,6 +12,20 @@ version(unittest) {
 		}
 	}
 	
+	interface InterfaceA {
+	}
+	
+	class ComponentC : InterfaceA {
+	}
+	
+	class ComponentD {
+		public @Autowire InterfaceA componentC;
+		
+		public bool componentIsNull() {
+			return componentC is null;
+		}
+	}
+	
 	// Test autowiring concrete type to existing instance
 	unittest {
 		auto container = new Container();
@@ -19,5 +33,14 @@ version(unittest) {
 		ComponentB componentB = new ComponentB();
 		container.autowire!(ComponentB)(componentB);
 		assert(!componentB.componentIsNull(), "Autowirable dependency failed to autowire");
+	}
+	
+	// Test autowiring interface type to existing instance
+	unittest {
+		auto container = new Container();
+		container.register!(InterfaceA, ComponentC);
+		ComponentD componentD = new ComponentD();
+		container.autowire!(ComponentD)(componentD);
+		assert(!componentD.componentIsNull(), "Autowirable dependency failed to autowire");
 	}
 }
