f4ad79003e11a41127d29afbcb92d313c22639d9
Mike Bierlee <m.bierlee@lostmoment.com>
2014-05-30 01:19:28 +0200

    Remove left-over debug import

--- source/poodinis/autowire.d
+++ source/poodinis/autowire.d
@@ -5,7 +5,6 @@ public import poodinis.container;
 class Autowire{};
 
 public void autowire(Type)(Container container, Type instance) {
-	import std.stdio;
 	foreach (member ; __traits(derivedMembers, Type)) {
 		foreach (attribute; mixin(`__traits(getAttributes, Type.` ~ member ~ `)`) ) {
 			if (is(attribute : Autowire) && __traits(getMember, instance, member) is null){
