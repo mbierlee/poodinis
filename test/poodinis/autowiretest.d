e5484edb19be3e33603e5de5ddfd8eb4d323f7ad
Mike Bierlee <m.bierlee@lostmoment.com>
2014-06-01 19:00:45 +0200

    Apply open-source MIT license

--- test/poodinis/autowiretest.d
+++ test/poodinis/autowiretest.d
@@ -1,3 +1,10 @@
+/**
+ * Poodinis Dependency Injection Framework
+ * Copyright 2014 Mike Bierlee
+ * This software is licensed under the terms of the MIT license.
+ * The full terms of the license can be found in the LICENSE file.
+ */
+
 import poodinis.autowire;
 
 import std.exception;
