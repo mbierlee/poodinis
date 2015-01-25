82ea4369b8fee9778c0e89bdf7a1526e15c7d8b9
Mike Bierlee <m.bierlee@lostmoment.com>
2015-01-25 13:40:11 +0100

    Add master module "dependency"

---
+++ source/poodinis/dependency.d
@@ -0,0 +1,12 @@
+/**
+ * Poodinis Dependency Injection Framework
+ * Copyright 2014 Mike Bierlee
+ * This software is licensed under the terms of the MIT license.
+ * The full terms of the license can be found in the LICENSE file.
+ */
+
+module poodinis.dependency;
+
+public import poodinis.autowire;
+public import poodinis.container;
+public import poodinis.registration;
