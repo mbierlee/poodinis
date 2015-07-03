a59e7a9731e24a6e9f39b621bb80671e72d3bcf6
Mike Bierlee <m.bierlee@lostmoment.com>
2015-07-04 00:30:14 +0200

    Add example project demonstrating @AssignNewInstance

---
+++ example/annotations/app.d
@@ -0,0 +1,43 @@
+import poodinis;
+
+import std.random;
+import std.digest.md;
+import std.stdio;
+import std.conv;
+
+class SuperSecurityDevice {
+	private int seed;
+
+	public this() {
+		auto randomGenerator = Random(unpredictableSeed);
+		seed = uniform(0, 999, randomGenerator);
+	}
+
+	public string getPassword() {
+		return to!string(seed) ~ "t1m3sp13!!:";
+	}
+}
+
+class SecurityManager {
+	@Autowire
+	public SuperSecurityDevice levelOneSecurity;
+
+	@Autowire
+	@AssignNewInstance
+	public SuperSecurityDevice levelTwoSecurity;
+}
+
+void main() {
+	auto dependencies = DependencyContainer.getInstance();
+	dependencies.register!SuperSecurityDevice; // Registered with the default "Single instance" scope
+	dependencies.register!SecurityManager;
+
+	auto manager = dependencies.resolve!SecurityManager;
+
+	writeln("Password for user one: " ~ manager.levelOneSecurity.getPassword());
+	writeln("Password for user two: " ~ manager.levelTwoSecurity.getPassword());
+
+	if (manager.levelOneSecurity is manager.levelTwoSecurity) {
+		writeln("SECURITY BREACH!!!!!");
+	}
+}
