664c51b9c444e032e26db680a74c19b44aab1097
Mike Bierlee <m.bierlee@lostmoment.com>
2015-12-28 22:45:15 +0100

    Add license to examples

--- example/arraycompletion/app.d
+++ example/arraycompletion/app.d
@@ -1,3 +1,10 @@
+/**
+ * Poodinis Dependency Injection Framework
+ * Copyright 2014-2015 Mike Bierlee
+ * This software is licensed under the terms of the MIT license.
+ * The full terms of the license can be found in the LICENSE file.
+ */
+
 import poodinis;
 
 import std.stdio;
