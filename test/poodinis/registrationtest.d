c35e494dfe7b78ba13135e27e1858c6dfb6d3a17
Mike Bierlee <m.bierlee@lostmoment.com>
2016-01-06 20:18:35 +0100

    Update copyrights

--- test/poodinis/registrationtest.d
+++ test/poodinis/registrationtest.d
@@ -1,6 +1,6 @@
 /**
  * Poodinis Dependency Injection Framework
- * Copyright 2014-2015 Mike Bierlee
+ * Copyright 2014-2016 Mike Bierlee
  * This software is licensed under the terms of the MIT license.
  * The full terms of the license can be found in the LICENSE file.
  */
