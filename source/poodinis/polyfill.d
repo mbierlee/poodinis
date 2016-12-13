8fe5b8da9b239085dda1174764c24463f58b2d21
Mike Bierlee <m.bierlee@lostmoment.com>
2016-12-13 23:22:55 +0100

    Adjust package documentation according extended use of derived work

--- source/poodinis/polyfill.d
+++ source/poodinis/polyfill.d
@@ -4,10 +4,22 @@
  *
  * Should not implement functionalitiy which is gone from the latest Phobos.
  *
- * The baseline compatibility is D/Phobos 2.068.0
+ * Implementations copied/re-implemented from std.traits, std.meta (std.typetuple)
+ * and std.exception
  *
- * Authors: $(HTTP erdani.org, Andrei Alexandrescu) Jonathan M Davis and Mike Bierlee (m.bierlee@lostmoment.com)
- * Copyright: Copyright Andrei Alexandrescu 2008-, Jonathan M Davis 2011-., 2014- Mike Bierlee
+ * The baseline compatibility is D/Phobos 2.066.1
+ *
+ * Authors: $(HTTP erdani.org, Andrei Alexandrescu),
+ *   Jonathan M Davis,
+ *   $(HTTP digitalmars.com, Walter Bright),
+ *   Tomasz Stachowiak ($(D isExpressions)),
+ *   Shin Fujishiro,
+ *   $(HTTP octarineparrot.com, Robert Clipsham),
+ *   $(HTTP klickverbot.at, David Nadlinger),
+ *   Kenji Hara,
+ *   Shoichi Kato,
+ *   Mike Bierlee (m.bierlee@lostmoment.com)
+ * Copyright: Copyright Digital Mars 2005 - 2015, Copyright Andrei Alexandrescu 2008-, Jonathan M Davis 2011-., 2014-2016 Mike Bierlee
  * License:  $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0)
  */
 
