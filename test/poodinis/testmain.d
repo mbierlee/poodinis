cc314324a78b3d62850a163da9aab2a4108ee5d6
Mike Bierlee <m.bierlee@lostmoment.com>
2016-12-13 22:28:12 +0100

    Use empty main instead of relying on main generation by compilers
    GDC doesn't seem to do this

---
+++ test/poodinis/testmain.d
@@ -0,0 +1,8 @@
+/**
+ * Poodinis Dependency Injection Framework
+ * Copyright 2014-2016 Mike Bierlee
+ * This software is licensed under the terms of the MIT license.
+ * The full terms of the license can be found in the LICENSE file.
+ */
+
+void main() {}
