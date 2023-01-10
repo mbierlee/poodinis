fd9d63a6ec0b68c4514b1c8b09b3ed02a70d7cc5
Mike Bierlee <m.bierlee@lostmoment.com>
2023-01-11 02:01:51 +0300

    Update copyrights

--- source/poodinis/package.d
+++ source/poodinis/package.d
@@ -3,7 +3,7 @@
  *
  * Authors:
  *  Mike Bierlee, m.bierlee@lostmoment.com
- * Copyright: 2014-2022 Mike Bierlee
+ * Copyright: 2014-2023 Mike Bierlee
  * License:
  *  This software is licensed under the terms of the MIT license.
  *  The full terms of the license can be found in the LICENSE file.
